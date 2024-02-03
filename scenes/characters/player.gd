134de1a6e07953ce43f236b8eb30cd44f7763862
bobertsanchezez <henry_earnest@brown.edu>
2024-02-03 17:56:48 -0500

    add player rotation towards planet of largest gravity

--- scenes/characters/player.gd
+++ scenes/characters/player.gd
@@ -1,28 +1,63 @@
-extends CharacterBody2D
-
+extends RigidBody2D
+class_name Player
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -40.0
+const JUMP_VELOCITY = -300.0
+
+var _planets = []
+# Planet with highest gravity
+var _target_planet : Planet
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+func _process(delta):
+	pass
+# Calculate actual gravity values from each planet, find largest
+func _calc_gravity_vector():
+	var largest_gravity = 0.0
+	if !_planets:
+		_target_planet = null
+	for planet in _planets:
+		var p_gravity = planet.gravity
+		var p_dist = planet.position.distance_to(self.position)
+		var p_unit_dist = planet.gravity_point_unit_distance
+		var p_eff_grav = pow(p_dist / p_unit_dist, 2) * p_gravity
+		if p_eff_grav > largest_gravity:
+			largest_gravity = p_eff_grav
+			_target_planet = planet 
+
+
+
+
+func add_planet(planet : Planet):
+	_planets.append(planet)
+
+func remove_planet(planet : Planet):
+	_planets.erase(planet)
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * 0.1 * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("move_left", "move_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	move_and_slide()
+	_calc_gravity_vector()
+	if _target_planet:
+		var rotate_speed = 1.0
+		rotation = lerp_angle(rotation, get_angle_to(_target_planet.position), delta * rotate_speed)
+	else: 
+		var rotate_speed = 1.0
+		rotation = lerp_angle(rotation, 0, delta * rotate_speed)
+	## Add the gravity.
+	#if not is_on_floor():
+		#velocity.y += gravity * 0.1 * delta
+#
+	## Handle jump.
+	#if Input.is_action_just_pressed("jump") and is_on_floor():
+		#velocity.y = JUMP_VELOCITY
+#
+	## Get the input direction and handle the movement/deceleration.
+	## As good practice, you should replace UI actions with custom gameplay actions.
+	#var direction = Input.get_axis("move_left", "move_right")
+	#if direction:
+		#velocity.x = direction * SPEED
+	#else:
+		#velocity.x = move_toward(velocity.x, 0, SPEED)
+#
+	#move_and_slide()
--- scenes/environment/planet.gd
+++ scenes/environment/planet.gd
@@ -1,5 +1,8 @@
 extends AnimatableBody2D
+class_name Planet
 
+var gravity := 0.0
+var gravity_point_unit_distance := 0.0;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -8,9 +11,14 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
-
-
+	gravity = $Area2D.gravity
+	gravity_point_unit_distance = $Area2D.gravity_point_unit_distance
+	
 
 func _on_area_2d_body_entered(body):
-	print("something entered")
+	if body.is_in_group("player") and body is Player:
+		body.add_planet(self)
+
+func _on_area_2d_body_exited(body):
+	if body.is_in_group("player") and body is Player:
+		body.remove_planet(self)
